a5c38626a8f47fc200b2c6480cd80580989e1a1a
Jose Uscanga <jousmo@outlook.com>
2020-05-18 14:41:42 -0500

    Example15 - Implement context with Apollo Server

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ require('dotenv').config()
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./lib/types')
 const resolvers = require('./lib/resolvers')
+const context = require('./lib/auth')
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, context })
 
 server.listen().then(async ({ url }) => {
   console.log(`Server running in: ${url}`)
